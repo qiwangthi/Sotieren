a19fdec38ef7dc45be9f401507bdc267ff8ecd4d
qiwangthi <164479452+qiwangthi@users.noreply.github.com>
2024-04-27 11:34:07 +0200

    Aufgabe1

---
+++ Sotieren/src/App.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // erzeugen Sie eine ArrayListe mit 500 zufälligen Integer-Werten im Bereich
+        // zwischen 1900 und 2013. Nutzen Sie dazu die statische Methode random der
+        // Klasse Math.
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        // ArrayList<Integer> list2 = new ArrayList<Integer>(list);
+        for (int i = 0; i < 500; i++) {
+            list.add((int) (Math.random() * 114) + 1900);
+
+        }
+        System.out.println(list);
+        // startTimermethode aufrufen
+
+    }
+}
---
+++ Sotieren/src/Sortierer.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Sortierer {
+    private List<Integer> liste;
+    private long startTime;
+    private long endTime;
+
+    // Der Konstruktor
+    public Sortierer(List<Integer> liste, long startTime, long endTime) {
+        this.liste = liste;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    // getter Methode
+    public List<Integer> getliste() {
+        return liste;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    // startTime
+    public void startTimer() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    // copy list
+    ArrayList<Integer> kopie = new ArrayList<Integer>(liste);
+
+    protected abstract List<Integer> sortiere(List<Integer> liste);
+
+    public List<Integer> sortiere() {
+        return sortiere(this.liste);
+    }
+
+    // endTime
+    public void endTimer() {
+        this.endTime = System.currentTimeMillis();
+    }
+
+    // differenz zwischen endTime und startTime
+    public long getOperations() {
+        return this.endTime - this.startTime;
+    }
+
+}
